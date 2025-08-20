2504eb24e1178116f1bfc9f2871c9ae9055614fc
YHH <359807859@qq.com>
2025-08-20 09:15:06 +0800

    经测试转换为wasm没有明显的效率提升，因为js和rust的交互反而有所下降 等待以后有密集计算再考虑使用，先移除了

--- packages/core-rust/src/storage/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod component_storage;
-pub mod soa_storage;
-
-pub use component_storage::{ComponentStorage, ComponentStorageManager};
-pub use soa_storage::{SoAStorage, SoAFieldType, SoAFieldMetadata, SoAStorageStats, SoAFieldStats};
