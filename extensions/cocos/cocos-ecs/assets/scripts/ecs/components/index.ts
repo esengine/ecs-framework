8011b5c8d2f53ef51d482729331f1a30ce3fe47c
YHH <359807859@qq.com>
2025-10-21 11:15:19 +0800

    chore: 移除 Cocos Creator 扩展目录

--- extensions/cocos/cocos-ecs/assets/scripts/ecs/components/index.ts
+++
@@ -1,8 +0,0 @@
-// 导出所有组件
-export { Transform } from './Transform';
-export { Health } from './Health';
-export { Velocity } from './Velocity';
-export { Renderer } from './Renderer';
-export { NodeComponent } from './NodeComponent';
-export { AIComponent } from './AIComponent';
-export { NetworkComponent } from './NetworkComponent'; 
