edbc9eb27f3ad86ee33a2c9ea446e0947f753373
YHH <359807859@qq.com>
2025-10-27 15:04:31 +0800

    ci(editor-core): 添加npm发布流程支持

---
+++ packages/editor-core/rollup.config.cjs
@@ -0,0 +1,132 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const terser = require('@rollup/plugin-terser');
+const babel = require('@rollup/plugin-babel');
+const dts = require('rollup-plugin-dts').default;
+const { readFileSync } = require('fs');
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+const banner = `/**
+ * @esengine/editor-core v${pkg.version}
+ * ECS Framework Editor Core - Plugin-based editor framework
+ *
+ * @author ${pkg.author}
+ * @license ${pkg.license}
+ */`;
+
+const external = ['@esengine/ecs-framework'];
+
+const commonPlugins = [
+  resolve({
+    browser: true,
+    preferBuiltins: false
+  }),
+  commonjs({
+    include: /node_modules/
+  }),
+  babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    extensions: ['.js', '.ts']
+  })
+];
+
+module.exports = [
+  // ES模块构建
+  {
+    input: 'bin/index.js',
+    output: {
+      file: 'dist/index.mjs',
+      format: 'es',
+      banner,
+      sourcemap: true,
+      exports: 'named'
+    },
+    plugins: [
+      ...commonPlugins,
+      terser({
+        format: {
+          comments: /^!/
+        }
+      })
+    ],
+    external,
+    treeshake: {
+      moduleSideEffects: false,
+      propertyReadSideEffects: false,
+      unknownGlobalSideEffects: false
+    }
+  },
+
+  // CommonJS构建
+  {
+    input: 'bin/index.js',
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      banner,
+      sourcemap: true,
+      exports: 'named'
+    },
+    plugins: [
+      ...commonPlugins,
+      terser({
+        format: {
+          comments: /^!/
+        }
+      })
+    ],
+    external,
+    treeshake: {
+      moduleSideEffects: false
+    }
+  },
+
+  // UMD构建
+  {
+    input: 'bin/index.js',
+    output: {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'EditorCore',
+      banner,
+      sourcemap: true,
+      exports: 'named',
+      globals: {
+        '@esengine/ecs-framework': 'ECS'
+      }
+    },
+    plugins: [
+      ...commonPlugins,
+      terser({
+        format: {
+          comments: /^!/
+        }
+      })
+    ],
+    external,
+    treeshake: {
+      moduleSideEffects: false
+    }
+  },
+
+  // 类型定义构建
+  {
+    input: 'bin/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+      banner: `/**
+ * @esengine/editor-core v${pkg.version}
+ * TypeScript definitions
+ */`
+    },
+    plugins: [
+      dts({
+        respectExternal: true
+      })
+    ],
+    external
+  }
+];
