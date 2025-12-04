c10de8b415724f0630ad44c9468f95faa419441c
yhh <359807859@qq.com>
2025-12-04 13:52:35 +0800

    feat: 实现资产 .meta 文件自动生成功能

--- packages/editor-app/src/plugins/builtin/index.ts
+++ packages/editor-app/src/plugins/builtin/index.ts
@@ -8,6 +8,7 @@ export { SceneInspectorPlugin } from './SceneInspectorPlugin';
 export { ProfilerPlugin } from './ProfilerPlugin';
 export { EditorAppearancePlugin } from './EditorAppearancePlugin';
 export { ProjectSettingsPlugin } from './ProjectSettingsPlugin';
+export { AssetMetaPlugin } from './AssetMetaPlugin';
 // Note: PluginConfigPlugin removed - module management is now unified in ProjectSettingsPlugin
 // TODO: Re-enable when blueprint-editor package is fixed
 // export { BlueprintPlugin } from '@esengine/blueprint-editor';
